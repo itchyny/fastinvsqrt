3280cde856c89b489281c2968dc30f48ca11ab85
itchyny <itchyny@hatena.ne.jp>
2016-07-08 22:24:02 +0900

    add Scala

---
+++ src/scala/FastInvSqrt.scala
@@ -0,0 +1,17 @@
+object FastInvSqrt {
+
+  def main(args: Array[String]) {
+    Iterator.continually(scala.io.StdIn.readLine()).takeWhile(_ != null).foreach { line =>
+      util.control.Exception.catching(classOf[NumberFormatException]) opt line.toFloat map { x =>
+        println(fastInvSqrt(x))
+      }
+    }
+  }
+
+  def fastInvSqrt(x: Float): Float = {
+    val i = java.lang.Float.floatToRawIntBits(x);
+    val y = java.lang.Float.intBitsToFloat(0x5f3759df - (i >> 1));
+    return y * (1.5F - 0.5F * x * y * y);
+  }
+
+}
