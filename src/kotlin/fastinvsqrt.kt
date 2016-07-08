b2dad53031b154eb516bfb00635b0a1884c3236e
itchyny <itchyny@hatena.ne.jp>
2016-07-08 23:00:02 +0900

    add Kotlin

---
+++ src/kotlin/fastinvsqrt.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+  while (true) {
+    val line = readLine();
+    if (line == null) break;
+    try {
+      println(fastInvSqrt(line.toFloat()));
+    } catch (e: NumberFormatException) {}
+  }
+}
+
+fun fastInvSqrt(x: Float): Float {
+  val i = java.lang.Float.floatToRawIntBits(x);
+  val y = java.lang.Float.intBitsToFloat(0x5f3759df - (i shr 1));
+  return y * (1.5F - 0.5F * x * y * y);
+}
