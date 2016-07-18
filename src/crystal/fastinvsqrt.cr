dc0be3fefcea5be309f311f2d5115966c767188b
itchyny <itchyny@hatena.ne.jp>
2016-07-18 22:11:46 +0900

    add Crystal

---
+++ src/crystal/fastinvsqrt.cr
@@ -0,0 +1,10 @@
+def fastinvsqrt(x : Float32) : Float32
+  i = (pointerof(x) as Int32*).value
+  i = 0x5f3759df - (i >> 1)
+  y = (pointerof(i) as Float32*).value
+  y * (1.5 - 0.5 * x * y * y)
+end
+
+while line = gets
+  puts fastinvsqrt(line.to_f32)
+end
