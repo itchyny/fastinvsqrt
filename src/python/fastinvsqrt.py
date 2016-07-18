4333bb0c9de4007580b61b6b6e989db54f32ede1
itchyny <itchyny@hatena.ne.jp>
2016-07-19 01:38:12 +0900

    improve Python

--- src/python/fastinvsqrt.py
+++ src/python/fastinvsqrt.py
@@ -2,9 +2,9 @@ import struct
 import sys
 
 def fastInvSqrt(x):
-    i = struct.unpack('>I', struct.pack('>f', x))[0]
+    i = struct.unpack('>i', struct.pack('>f', x))[0]
     i = 0x5f3759df - (i >> 1);
-    y = struct.unpack('>f', struct.pack('>I', i))[0]
+    y = struct.unpack('>f', struct.pack('>i', i))[0]
     return y * (1.5 - 0.5 * x * y * y)
 
 if __name__ == '__main__':
