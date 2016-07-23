34cffe9695eb7754904bcbf9fdb14d89087acdd3
itchyny <itchyny@hatena.ne.jp>
2016-07-24 03:11:34 +0900

    add Swift

---
+++ src/swift/fastinvsqrt.swift
@@ -0,0 +1,13 @@
+func fastInvSqrt(x: Float) -> Float {
+    var z: Float = x
+    var i: Int = UnsafeMutablePointer<Int>(withUnsafeMutablePointer(&z, { $0 })).memory
+    i = 0x5f3759df - (i >> 1)
+    let y: Float = UnsafeMutablePointer<Float>(withUnsafeMutablePointer(&i, { $0 })).memory
+    return y * (1.5 - 0.5 * x * y * y)
+}
+
+while let line = readLine() {
+    if let x = Float(line) {
+        print(fastInvSqrt(x))
+    }
+}
