fff649e2a65e4daa3229106e995fcbe950d7578c
itchyny <itchyny@hatena.ne.jp>
2016-07-24 11:02:08 +0900

    improve F#

--- src/fsharp/FastInvSqrt.fs
+++ src/fsharp/FastInvSqrt.fs
@@ -4,7 +4,7 @@ let fastinvsqrt(x: float32): float32 =
     let y = System.BitConverter.ToSingle(System.BitConverter.GetBytes(j), 0)
     y * (1.5F - 0.5F * x * y * y)
 
-seq { while true do yield stdin.ReadLine () }
+seq { while true do yield stdin.ReadLine() }
     |> Seq.takeWhile ((<>) null)
     |> Seq.iter (fun x -> try float32 x |> fastinvsqrt |> printfn "%f"
                           with :? System.FormatException -> ())
