92bb3b95fd7b158872b0ebd5b2aa0d7d3b60eab2
itchyny <itchyny@hatena.ne.jp>
2016-07-24 22:30:21 +0900

    improve R

--- src/r/fastinvsqrt.r
+++ src/r/fastinvsqrt.r
@@ -1,18 +1,8 @@
 fastInvSqrt <- function(x) {
-  i <- floatToInteger(x)
+  i <- readBin(writeBin(as.numeric(x), raw(), size=4), integer(), size=4)
   i <- 0x5f3759df - bitwShiftR(i, 1)
-  y <- integerToFloat(i)
+  y <- readBin(writeBin(as.integer(i), raw(), size=4), double(), size=4)
   y * (1.5 - 0.5 * x * y * y)
-}
-
-floatToInteger <- function(x) {
-  v <- writeBin(as.numeric(x), raw(), size=4)
-  readBin(v, integer(), size=4)
-}
-
-integerToFloat <- function(i) {
-  v <- writeBin(as.integer(i), raw(), size=4)
-  readBin(v, double(), size=4)
 }
 
 f <- file("stdin")
