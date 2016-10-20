21a069285b06567a5356cae4424652ccda278cff
itchyny <itchyny@hatena.ne.jp>
2016-10-20 20:23:51 +0900

    fix Crystal code

--- src/crystal/fastinvsqrt.cr
+++ src/crystal/fastinvsqrt.cr
@@ -1,7 +1,7 @@
 def fastinvsqrt(x : Float32) : Float32
-  i = (pointerof(x) as Int32*).value
+  i = pointerof(x).as(Int32*).value
   i = 0x5f3759df - (i >> 1)
-  y = (pointerof(i) as Float32*).value
+  y = pointerof(i).as(Float32*).value
   y * (1.5 - 0.5 * x * y * y)
 end
 
