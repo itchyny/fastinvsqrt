6bcf27976d40a4d1851fd5425742914443bc52a5
itchyny <itchyny@hatena.ne.jp>
2018-08-20 08:58:15 +0900

    fix missing argument label for Swift 3.0

--- src/swift/fastinvsqrt.swift
+++ src/swift/fastinvsqrt.swift
@@ -1,13 +1,13 @@
 func fastInvSqrt(x: Float) -> Float {
     var z: Float = x
-    var i: Int = UnsafeMutablePointer<Int>(withUnsafeMutablePointer(&z, { $0 })).memory
+    var i: Int = UnsafeMutablePointer<Int>(withUnsafeMutablePointer(to: &z, { $0 })).memory
     i = 0x5f3759df - (i >> 1)
-    let y: Float = UnsafeMutablePointer<Float>(withUnsafeMutablePointer(&i, { $0 })).memory
+    let y: Float = UnsafeMutablePointer<Float>(withUnsafeMutablePointer(to: &i, { $0 })).memory
     return y * (1.5 - 0.5 * x * y * y)
 }
 
 while let line = readLine() {
     if let x = Float(line) {
-        print(fastInvSqrt(x))
+        print(fastInvSqrt(x: x))
     }
 }
