ef4f2da25ea5d9ce595c1534ea6275d78ac627cb
itchyny <itchyny@hatena.ne.jp>
2018-08-22 01:30:33 +0900

    update PureScript: use psc-package and update libraries

--- src/purescript/FastInvSqrt.purs
+++ src/purescript/FastInvSqrt.purs
@@ -1,40 +1,38 @@
 module Main where
 
-import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, logShow)
-import Control.Monad.Eff.Exception (EXCEPTION)
 import Data.ArrayBuffer.ArrayBuffer (create)
-import Data.ArrayBuffer.DataView (READER, WRITER, whole, getInt32, getFloat32, setInt32, setFloat32)
+import Data.ArrayBuffer.DataView (whole, getInt32be, getFloat32be, setInt32be, setFloat32be)
 import Data.Int.Bits (shr)
 import Data.Maybe (fromJust)
-import Data.Monoid (mempty)
+import Effect (Effect)
+import Effect.Console (logShow)
 import Global (readFloat)
 import Node.Process (stdin)
-import Node.ReadLine (READLINE, setLineHandler, createInterface)
+import Node.ReadLine (setLineHandler, createInterface)
 import Partial.Unsafe (unsafePartial)
+import Prelude
 
-main :: forall e. Eff (console :: CONSOLE, readline :: READLINE, err :: EXCEPTION, reader :: READER, writer :: WRITER | e) Unit
+main :: Effect Unit
 main = do
   interface <- createInterface stdin mempty
   setLineHandler interface $ \line ->
     logShow =<< fastInvSqrt (readFloat line)
 
-fastInvSqrt :: forall e. Number -> Eff (reader :: READER, writer :: WRITER | e) Number
+fastInvSqrt :: Number -> Effect Number
 fastInvSqrt x = do
   i <- floatToUInt32 x
   let j = 0x5f3759df - unsafePartial i `shr` 1
   y <- uint32ToFloat j
   pure $ y * (1.5 - 0.5 * x * y * y)
 
-floatToUInt32 :: forall e. Number -> Eff (reader :: READER, writer :: WRITER | e) Int
+floatToUInt32 :: Number -> Effect Int
 floatToUInt32 x = do
-  let dataView = whole $ create 4
-  setFloat32 dataView x 0
-  unsafePartial $ map fromJust $ getInt32 dataView 0
+  dataView <- map whole $ create 4
+  setFloat32be dataView x 0
+  unsafePartial $ map fromJust $ getInt32be dataView 0
 
-uint32ToFloat :: forall e. Int -> Eff (reader :: READER, writer :: WRITER | e) Number
+uint32ToFloat :: Int -> Effect Number
 uint32ToFloat i = do
-  let dataView = whole $ create 4
-  setInt32 dataView i 0
-  unsafePartial $ map fromJust $ getFloat32 dataView 0
+  dataView <- map whole $ create 4
+  setInt32be dataView i 0
+  unsafePartial $ map fromJust $ getFloat32be dataView 0
