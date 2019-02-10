22513c34dcabb9c8151c12e655e768d7aae09865
masklinn <github.com@masklinn.net>
2019-02-10 15:12:42 +0100

    Remove unsafe calls from program
    Rust 1.20 added f32::from_bits and f32::to_bits (and the same on f64), which do exactly what we'd want.

--- src/rust/fastinvsqrt.rs
+++ src/rust/fastinvsqrt.rs
@@ -1,9 +1,9 @@
 use std::io::BufRead;
 
 fn fast_inv_sqrt(x: f32) -> f32 {
-    let i: u32 = unsafe { std::mem::transmute(x) };
+    let i = x.to_bits();
     let j = 0x5f3759df - (i >> 1);
-    let y: f32 = unsafe { std::mem::transmute(j) };
+    let y = f32::from_bits(j);
     y * (1.5 - 0.5 * x * y * y)
 }
 
