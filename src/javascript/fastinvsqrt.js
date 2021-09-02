227bf0d38ab84440e22c5dec425001f97821b277
wintus <6386129+wintus@users.noreply.github.com>
2021-09-03 04:15:16 +0900

    Use ES2015 variable declaration

--- src/javascript/fastinvsqrt.js
+++ src/javascript/fastinvsqrt.js
@@ -5,20 +5,20 @@ require('readline').createInterface({
 });
 
 function fastInvSqrt(x) {
-    var i = floatToUInt32(x);
+    let i = floatToUInt32(x);
     i = 0x5f3759df - (i >> 1);
-    var y = uint32ToFloat(i);
+    const y = uint32ToFloat(i);
     return y * (1.5 - 0.5 * x * y * y);
 }
 
 function floatToUInt32(x) {
-    var buf = new ArrayBuffer(4);
+    const buf = new ArrayBuffer(4);
     new Float32Array(buf)[0] = x;
     return new Uint32Array(buf)[0];
 }
 
 function uint32ToFloat(i) {
-    var buf = new ArrayBuffer(4);
+    const buf = new ArrayBuffer(4);
     new Uint32Array(buf)[0] = i;
     return new Float32Array(buf)[0];
 }
