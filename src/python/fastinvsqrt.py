ed972283e8d88645c135bb062f5f143bbedba3f6
itchyny <itchyny@hatena.ne.jp>
2021-09-12 19:37:02 +0900

    clean up Python

--- src/python/fastinvsqrt.py
+++ src/python/fastinvsqrt.py
@@ -1,11 +1,13 @@
 import struct
 import sys
 
+
 def fastInvSqrt(x):
     i = struct.unpack('>i', struct.pack('>f', x))[0]
-    i = 0x5f3759df - (i >> 1);
+    i = 0x5f3759df - (i >> 1)
     y = struct.unpack('>f', struct.pack('>i', i))[0]
     return y * (1.5 - 0.5 * x * y * y)
+
 
 if __name__ == '__main__':
     for line in iter(sys.stdin.readline, ""):
