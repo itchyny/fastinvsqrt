e7968f5ba5d969c10e0675fbbd31704e06b52369
itchyny <itchyny@hatena.ne.jp>
2021-09-12 13:58:16 +0900

    refactor TypeScript

--- src/typescript/fastinvsqrt.ts
+++ src/typescript/fastinvsqrt.ts
@@ -1,28 +1,25 @@
-declare var require;
-declare var process;
+import * as process from "process"
+import * as readline from "readline"
 
-require('readline').createInterface({
+readline.createInterface({
     input: process.stdin,
-    output: process.null
-}).on('line', function(line) {
+}).on('line', function(line: string) {
     console.log(fastInvSqrt(parseFloat(line)));
 });
 
 function fastInvSqrt(x: number): number {
-    var i: number = floatToUInt32(x);
-    i = 0x5f3759df - (i >> 1);
-    var y: number = uint32ToFloat(i);
+    const i: number = floatToUInt32(x);
+    const j: number = 0x5f3759df - (i >> 1);
+    const y: number = uint32ToFloat(j);
     return y * (1.5 - 0.5 * x * y * y);
 }
 
 function floatToUInt32(x: number): number {
-    var buf: ArrayBuffer = new ArrayBuffer(4);
-    new Float32Array(buf)[0] = x;
+    const buf: ArrayBuffer = Float32Array.of(x).buffer;
     return new Uint32Array(buf)[0];
 }
 
 function uint32ToFloat(i: number): number {
-    var buf: ArrayBuffer = new ArrayBuffer(4);
-    new Uint32Array(buf)[0] = i;
+    const buf: ArrayBuffer = Uint32Array.of(i).buffer;
     return new Float32Array(buf)[0];
 }
