cfb84c97b153ea456833335021c3ae0d2e694eed
itchyny <itchyny@cybozu.co.jp>
2022-09-25 20:37:15 +0900

    upgrade Scala

--- src/scala/FastInvSqrt.scala
+++ src/scala/FastInvSqrt.scala
@@ -1,17 +1,12 @@
-object FastInvSqrt {
+object FastInvSqrt:
 
-  def main(args: Array[String]) {
-    Iterator.continually(scala.io.StdIn.readLine()).takeWhile(_ != null).foreach { line =>
-      util.control.Exception.catching(classOf[NumberFormatException]) opt line.toFloat map { x =>
+  def main(args: Array[String]) =
+    for (line <- io.Source.stdin.getLines)
+      line.toFloatOption.map { x =>
         println(fastInvSqrt(x))
       }
-    }
-  }
 
-  def fastInvSqrt(x: Float): Float = {
+  def fastInvSqrt(x: Float): Float =
     val i = java.lang.Float.floatToRawIntBits(x)
     val y = java.lang.Float.intBitsToFloat(0x5f3759df - (i >> 1))
     y * (1.5F - 0.5F * x * y * y)
-  }
-
-}
