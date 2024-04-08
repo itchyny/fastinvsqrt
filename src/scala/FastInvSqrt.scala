9019f41b10990b3ac4f026d12db6d902efd45e9b
itchyny <itchyny@cybozu.co.jp>
2024-04-09 07:57:59 +0900

    improve Scala using main method

--- src/scala/FastInvSqrt.scala
+++ src/scala/FastInvSqrt.scala
@@ -1,12 +1,9 @@
-object FastInvSqrt:
+@main def main(): Unit =
+  for (line <- io.Source.stdin.getLines)
+    for x <- line.toFloatOption
+    do println(fastInvSqrt(x))
 
-  def main(args: Array[String]) =
-    for (line <- io.Source.stdin.getLines)
-      line.toFloatOption.map { x =>
-        println(fastInvSqrt(x))
-      }
-
-  def fastInvSqrt(x: Float): Float =
-    val i = java.lang.Float.floatToRawIntBits(x)
-    val y = java.lang.Float.intBitsToFloat(0x5f3759df - (i >> 1))
-    y * (1.5F - 0.5F * x * y * y)
+def fastInvSqrt(x: Float): Float =
+  val i = java.lang.Float.floatToRawIntBits(x)
+  val y = java.lang.Float.intBitsToFloat(0x5f3759df - (i >> 1))
+  y * (1.5F - 0.5F * x * y * y)
